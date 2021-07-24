13afa403c9238b6d97df450643ba48997bd3eaee
Hanna Lee <8annahxxl@gmail.com>
2021-07-24 13:05:12 +0900

    E2E testing movies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,8 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // 각각의 테스트가 진행될 때마다 애플리케이션이 새로 생성되지 않게 하기 위해 beforeEach -> beforeAll
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -19,6 +20,30 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()) //
+        .get('/movies') //
+        .expect(200) //
+        .expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()) //
+        .delete('/movies') //
+        .expect(404);
+    });
   });
 });
